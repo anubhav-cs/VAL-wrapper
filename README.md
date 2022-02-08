36748934f3f6239170d273bfd50cd35a5c0ac7eb
zura <anubhav.singh.eng@gmail.com>
2022-02-08 21:13:20 +1100

    Test pypi build

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ How does it work?
 The entire build and python packaging is handled by cmake. The VAL github source is added as an external project which is compiled on the target system. The python wrapper is a script which calls the Validate executable.
 
 LICENSE
+-------
 The *wrapper* source code in this repository is released under MIT license. On the other hand, the pypi release, which redistributes the VAL binaries carries the original [license](https://github.com/KCL-Planning/VAL/blob/3c7a1f330bdab0ba28a4762bb45c3f06c27fb6d4/LICENSE) under which VAL source is released.
 
 # BUILD Commands
--- config/pypi/setup.py
+++ config/pypi/setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
     project_description = fh.read()
 
 
-# try:
-#     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-#     class bdist_wheel(_bdist_wheel):
-#         def finalize_options(self):
-#             _bdist_wheel.finalize_options(self)
-#             self.root_is_pure = False
-# except ImportError:
-#     bdist_wheel = None
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class bdist_wheel(_bdist_wheel):
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            self.root_is_pure = False
+except ImportError:
+    bdist_wheel = None
 
 
 setup(
