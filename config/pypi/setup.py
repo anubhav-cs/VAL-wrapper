5d464885c77e38dcfb2f2bdf9b556dbb2e17b616
zura <anubhav.singh.eng@gmail.com>
2022-02-09 02:06:48 +1100

    Test build

--- config/pypi/setup.py
+++ config/pypi/setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name            =   "val-wrapper",
-    version         =   "0.1.3",
+    version         =   "0.1.4",
     author          =   "Anubhav Singh",
     author_email    =   "anubhav.singh.er@pm.me",
     description     =   "plan validation toolkit",
