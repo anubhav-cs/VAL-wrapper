78adb93f7a5413aba769c48397024ff03e2ce23c
zura <anubhav.singh.eng@gmail.com>
2022-02-09 20:35:15 +1100

    Adding toolchain cmake file

---
+++ config/toolchain/toolchain_mingw_x86_64.cmake
@@ -0,0 +1,6 @@
+message(STATUS "USING MinGW x86_64")
+
+set(CMAKE_GENERATOR "MinGW Makefiles")
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
